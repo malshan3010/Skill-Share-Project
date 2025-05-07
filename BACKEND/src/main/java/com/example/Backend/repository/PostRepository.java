73e49cf6da122ac18f3fc01261d9e6e53a7356a2
SanjanaSandaruwan <sanjana2002max@gmail.com>
2025-05-07 23:03:00 +0530

    update post repo

---
+++ BACKEND/src/main/java/com/example/Backend/repository/PostRepository.java
@@ -0,0 +1,17 @@
+package com.example.Backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Backend.model.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findByUserId(String userId);
+
+    List<Post> findAllByOrderByCreatedAtDesc();
+
+    List<Post> findByUserIdOrderByCreatedAtDesc(String userId);
+}
